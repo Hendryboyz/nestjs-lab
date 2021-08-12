3a3f3796fee4302efc6fcc4dfb6a1c509b1b5f65
Hendryboyz <Hendry.js1247@gmail.com>
2021-08-12 15:15:01 +0800

    Extract cats API to a module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
-import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  controllers: [CatsController],
-  providers: [CatsService]
+  imports: [CatsModule]
 })
 export class AppModule {}
